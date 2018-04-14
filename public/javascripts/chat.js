31c7fcf4bdf4f1cda1986800dca32022da0db7f1
sagar malatesh <sagar.n.malatesh@gmail.com>
2018-04-14 12:12:43 +0000

    halfwaythrough

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,4 +1,22 @@
 var app = angular.module('chatapp', ['ngMaterial']);
 app.controller('chatController', function ($scope) {
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'Hello'
+        },
+        {
+            'sender': 'BOT',
+            'text': 'hi ssup!'
+        },
+        {
+            'sender': 'USER',
+            'text': 'the roof'
+              },
+        {
+            'sender': 'BOT',
+            'text': 'but i see sky'
+              }
 
+         ];
 });
