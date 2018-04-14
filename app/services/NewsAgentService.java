55324d6de158656bd58ba7088adeea568f092982
sagar malatesh <sagar.n.malatesh@gmail.com>
2018-04-14 06:05:37 +0000

    added newsagentservice

--- app/services/NewsAgentService.java
+++ app/services/NewsAgentService.java
@@ -5,11 +5,43 @@ import data.NewsAgentResponse;
 import play.libs.ws.WS;
 import play.libs.ws.WSRequest;
 import play.libs.ws.WSResponse;
-
+import scala.util.control.Exception;
 import java.util.UUID;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
+import static scala.util.control.Exception.*;
+
 public class NewsAgentService {
+   public NewsAgentResponse getNewsAgentResponse(String message,UUID sessionId){
+
+     NewsAgentResponse newsAgentResponse = new NewsAgentResponse();
+
+     try{
+         WSRequest queyRequest =WS.url("https://api.api.ai/api/query");
+         CompletionStage<WSResponse> responsePromise = queryRequest
+                 .setQueryParameter("v","20150910");
+                 .setQueryParameter("query","news about putin");
+                 .setQueryParameter("lang","en");
+                 .setQueryParameter("sessionId",sessionId.toString());
+                 .setQueryParameter("timezone","2018-13-04T16:57:23+0530");
+                 .setHeader("Authorization","Bearer 054a388ef08e46c3beb61cd9a12dd13f");
+                 .get();
+         JsonNode response =responsePromise.thenApply(WSResponse::asJson).toCompletableFuture().get();
+
+         newsAgentResponse.query = response.get("result").get("parameters").get("keyword").asText().isEmpty() ?
+                 (response.get("result").get("parameters").get("source").asText().isEmpty()
+                         ? response.get("result").get("parameters").get("category").asText()
+                         : response.get("result").get("parameters").get("source").asText() )
+                 : response.get("result").get("parameters").get("keyword").asText() ;
+
+     } catch (Exception e){
+           e.PintStackTrace("exception");
+
+       }
+       return newsAgentResponse;
+
+   }
+
 
 }
